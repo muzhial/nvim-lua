01a949cc29bb11ffed0c55fb7b5dd46478ba65cd
muzhi <kantchenjn@163.com>
2022-04-23 11:56:59 +0800

    init commit

---
+++ lua/plugin-config/dashboard.lua
@@ -0,0 +1,40 @@
+vim.g.dashboard_default_executive = "telescope"
+vim.g.dashboard_custom_footer = { "https://github.com/nshen/learn-neovim-lua" }
+
+vim.g.dashboard_custom_section = {
+  a = { description = { "  Projects              " }, command = "Telescope projects" },
+  b = { description = { "  Recently files        " }, command = "Telescope oldfiles" },
+  c = { description = { "  Edit keybindings      " }, command = "edit ~/.config/nvim/lua/keybindings.lua" },
+  d = {
+    description = { "  Edit Projects         " },
+    command = "edit ~/.local/share/nvim/project_nvim/project_history",
+  },
+  -- e = { description = { "  Edit .bashrc          " }, command = "edit ~/.bashrc" },
+  -- f = { description = { "  Change colorscheme    " }, command = "ChangeColorScheme" },
+  -- g = { description = { "  Edit init.lua         " }, command = "edit ~/.config/nvim/init.lua" },
+  -- h = { description = {'  Find file          '}, command = 'Telescope find_files'},
+  -- i = { description = {'  Find text          '}, command = 'Telescope live_grep'},
+}
+
+vim.g.dashboard_custom_header = {
+  [[          ▀████▀▄▄              ▄█ ]],
+  [[            █▀    ▀▀▄▄▄▄▄    ▄▄▀▀█ ]],
+  [[    ▄        █          ▀▀▀▀▄  ▄▀  ]],
+  [[   ▄▀ ▀▄      ▀▄              ▀▄▀  ]],
+  [[  ▄▀    █     █▀   ▄█▀▄      ▄█    ]],
+  [[  ▀▄     ▀▄  █     ▀██▀     ██▄█   ]],
+  [[   ▀▄    ▄▀ █   ▄██▄   ▄  ▄  ▀▀ █  ]],
+  [[    █  ▄▀  █    ▀██▀    ▀▀ ▀▀  ▄▀  ]],
+  [[   █   █  █      ▄▄           ▄▀   ]],
+}
+
+vim.g.dashboard_custom_header = {
+  [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗]],
+  [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║]],
+  [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║]],
+  [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║]],
+  [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║]],
+  [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝]],
+  [[                                                   ]],
+  [[                [ fucking world ]                  ]],
+}
